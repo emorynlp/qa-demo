63454a677f541188764d50777ad4433764ff7dee
Tomasz Jurczyk <tomasz@tomaszjurczyk.com>
2016-10-04 11:35:53 -0400

    Added scripts for installation.

---
+++ prepare.sh
@@ -0,0 +1,16 @@
+echo "{" > config.json
+echo "  \"backend_host\": \"127.0.0.1\"," >> config.json
+echo "  \"backend_port\": \"9500\"," >> config.json
+echo "" >> config.json
+echo "  \"frontend_host\": \"127.0.0.1\"," >> config.json
+echo "  \"frontend_port\": \"5000\"," >> config.json
+echo "" >> config.json
+echo "  \"frontend_key\": \"<ENTER_YOUR_KEY>\"," >> config.json
+echo "" >> config.json
+echo "  \"elasticsearch_host\": \"127.0.0.1\"," >> config.json
+echo "  \"elasticsearch_port\": \"9200\"," >> config.json
+echo "  \"elasticsearch_index\": \"wikipedia_08\"," >> config.json
+echo "  \"elasticsearch_type\": \"paragraph\"," >> config.json
+echo "" >> config.json
+echo "  \"word2vec_bin_file\": \"/Volumes/Transcend/Downloads/GoogleNews-vectors-negative300.bin\"" >> config.json
+echo "}" >> config.json
