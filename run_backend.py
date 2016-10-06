44fe93a04e1e172f13fb32710ec13f91e1849445
Tomasz Jurczyk <tomasz@tomaszjurczyk.com>
2016-10-06 00:54:42 -0400

    Minor change in paths

--- run_backend.py
+++ run_backend.py
@@ -9,6 +9,7 @@ settings = json.load(open('config.json'))
 if __name__ == '__main__':
     backend = ATBackendServer(settings['elasticsearch_host'], settings['elasticsearch_index'],
                               settings['elasticsearch_type'],
-                              ['at_cnnmodel.model', 'at_lrmodel.model', 'at_idf.model'])
+                              ['pretrained_models/at_cnnmodel.model', 'pretrained_models/at_lrmodel.model', 
+                               'pretrained_models/at_idf.model'])
     run_simple(settings['backend_host'], settings['backend_port'], backend.application)
     
